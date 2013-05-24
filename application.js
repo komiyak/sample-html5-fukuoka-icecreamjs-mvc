d28b7f73af6bf564dba5245c109fbcaf89b6b396
Kosuke Komiya <komiya@isoftware.jp>
2013-05-24 16:55:13 +0900

    選択した要素を、選択リストとして表示していくようにした。

---
+++ application.js
@@ -0,0 +1,71 @@
+
+// アイスクリームの種類
+var icecream = [
+    {id: "t1", name: "バニラ"},
+    {id: "t2", name: "チョコレート"},
+    {id: "t3", name: "オレンジ"},
+];
+
+// 選択されたアイスクリーム
+var selectedIcecream = [];
+
+
+function findById(obj, id) {
+    var ret = null;
+
+    if (obj) {
+        $.each(obj, function(index, value) {
+            if (value.id === id) {
+                ret = value;
+                return false; // 中断
+            }
+        });
+    }
+    
+    return ret;
+}
+
+
+function updateViewIcecreamList() {
+    var list = $("#icecream-list");
+
+    list.empty(); // 一度空っぽへ
+    
+    $.each( selectedIcecream, function(index, value) {
+        list.append( $("<p>").text( index + ': ' + value.name ) );
+    });
+}
+
+
+// entry point
+$(function() {
+
+  // 最初のビューを作成する
+  var base = $("#icecream");
+  
+  $.each( icecream, function(index, element) {
+  
+      // checkbox の click event
+      var clickevent = function(event) {
+          var name = event.currentTarget.name;
+          
+          // 検索
+          var ice = findById( icecream, name );
+          
+          
+          selectedIcecream.push( ice );
+          
+          // ビューの更新
+          updateViewIcecreamList()
+      };
+  
+      // 要素作成
+      base.append(
+          $("<li>")
+              .append( $("<input type='checkbox'>").attr("name", element.id).click( clickevent ) )
+              .append( $("<span>").text(element.name) )
+      );
+  });
+
+});
+// EOF
